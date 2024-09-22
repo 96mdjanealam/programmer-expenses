3f5b8aa6cc95a9d16d943ef6b4a4710d251e3568
Md Jane Alam <96mdjanealam@gmail.com>
2024-09-22 11:20:06 +0600

    tentative final 1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Utilities
 function getInputNumberById(id) {
     const value = document.getElementById(id).value;
 
@@ -16,7 +16,22 @@ function getInputNumberById(id) {
     return number;
 }
 
+function showSection(id) {
+    document.getElementById("assistant-section").classList.add("hidden");
+    document.getElementById("history-section").classList.add("hidden");
 
+    document.getElementById(id).classList.remove("hidden");
+}
+
+// Tab actions
+document.getElementById("assistant").addEventListener("click", function(){
+    showSection("assistant-section");
+});
+document.getElementById("history").addEventListener("click", function(){
+    showSection("history-section");
+});
+
+// Calculate button action
 document.getElementById("calculate").addEventListener("click", function () {
     const income = getInputNumberById("income");
     const software = getInputNumberById("software");
@@ -31,14 +46,44 @@ document.getElementById("calculate").addEventListener("click", function () {
     const totalExpenses = software + courses + internet;
     const balance = income - totalExpenses;
 
+    document.getElementById("total-expenses").innerText = totalExpenses;
     document.getElementById("balance").innerText = balance;
 });
 
+// Calculate Savings button action
 document.getElementById("calculate-saving").addEventListener("click", function () {
     const savingPercentage = getInputNumberById("saving");
+
+    if (savingPercentage === false) {
+        return alert ("Please enter valid inputs.");
+    }
+
     const balance = parseFloat(document.getElementById("balance").innerText);
     const savingAmount = (savingPercentage * balance) / 100;
+
     document.getElementById("saving-amount").innerText = savingAmount;
+
     const remainingBalance = balance - savingAmount;
+    
     document.getElementById("remaining-balance").innerText = remainingBalance;
+
+    const income = getInputNumberById("income");
+    const expenses = parseFloat(document.getElementById("total-expenses").innerText);
+
+    let currentDate = new Date();
+    let cDate = currentDate.toLocaleDateString();
+    let cTime = currentDate.toLocaleTimeString();
+
+    const newDiv = document.createElement("div");
+    
+    newDiv.innerHTML = `
+        <p class="text-sm">Date: ${cDate} Time: ${cTime}</p>
+        <p>Income: <span class="font-bold">${income}</span> Taka</p>
+        <p>Expenses: <span class="font-bold">${expenses}</span> Taka</p>
+        <p>Saving: <span class="font-bold">${savingAmount}</span> Taka</p>
+        <p>Balance: <span class="font-bold">${remainingBalance}</span> Taka</p>
+    `
+    document.getElementById("history-items").appendChild(newDiv);
+
+    document.getElementById("no-history").classList.add("hidden");
 })
